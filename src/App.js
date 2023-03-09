242a755a5478ac4a56633cab40a66c0988daed73
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-09 17:19:29 -0500

    addition of carousel bones

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Button>IS THIS BETTER THAN CSS ?</Button>
+      <Button>Click To Connect</Button>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,33 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
 function Header() {
 
   return (
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src='./images/UNCOR.jpeg' class="d-block w-100" alt="UNC OR Staff Thanksgiving 2020" />
-            </div>
-        <div class="carousel-item">
-                <img src="./images/glacierBridge.jpeg" class="d-block w-100" alt="Glacier National Park 2022" />
-            </div>
-        </div>
-            <div class="carousel-item">
-                <img src="./images/templeOfTheMoon.jpeg" class="d-block w-100" alt="Temple of The Moon 2022" />
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+    <Carousel>
+        <Carousel.Item>
+             <img 
+                src="./images/grandCanyonAyo.jpeg"
+                alt="Grand Canyon 2022" 
+            />
+            <img 
+                src='./images/UNCOR.jpeg' 
+                alt="UNC OR Staff Thanksgiving 2020" 
+            />
+              <img 
+                src="./images/templeOfTheMoon.jpeg"
+                alt="Temple Of The Moon 2022" 
+            />
+             <img 
+                src="./images/glacierBridge.jpeg"
+                alt="Glacier National Park 2022" 
+            />
+
+            <Carousel.Caption style={{color:"white"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
   )
 }
 
