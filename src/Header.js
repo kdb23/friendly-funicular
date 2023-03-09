728087239a49ed1ed49888afe72edeaa83f4a479
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-09 17:30:56 -0500

    modifications to carousel

--- src/Header.js
+++ src/Header.js
@@ -7,23 +7,73 @@ function Header() {
     <Carousel>
         <Carousel.Item>
              <img 
-                src="./images/grandCanyonAyo.jpeg"
+                src="./images/lakeLouiseCanoe.jpeg"
                 alt="Grand Canyon 2022" 
             />
+            </Carousel.Item>
+            <Carousel.Caption style={{color:"black"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        <Carousel.Item>
             <img 
                 src='./images/UNCOR.jpeg' 
                 alt="UNC OR Staff Thanksgiving 2020" 
             />
+            </Carousel.Item>
+            <Carousel.Caption style={{color:"black"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        <Carousel.Item>
               <img 
                 src="./images/templeOfTheMoon.jpeg"
                 alt="Temple Of The Moon 2022" 
             />
+            </Carousel.Item>
+            <Carousel.Caption style={{color:"black"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        <Carousel.Item>
              <img 
-                src="./images/glacierBridge.jpeg"
+                src="./images/grandPrismaticSpring.jpeg"
                 alt="Glacier National Park 2022" 
             />
-
-            <Carousel.Caption style={{color:"white"}}>
+            </Carousel.Item>
+            <Carousel.Caption style={{color:"black"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        <Carousel.Item>
+             <img 
+                src="./images/banffWildflowers.jpeg"
+                alt="Bow Valley 2022" 
+            />
+            </Carousel.Item>
+            <Carousel.Caption style={{color:"black"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        <Carousel.Item>
+            <img 
+                src="./images/bowLake.jpeg"
+                alt="Bow Lake 2022" 
+            />
+            </Carousel.Item>
+            <Carousel.Caption style={{color:"black"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        <Carousel.Item>
+              <img 
+                src="./images/delicateArch.jpeg"
+                alt="Arches National 2022" 
+            />
+            </Carousel.Item>
+            <Carousel.Caption style={{color:"black"}}>
+                <h1> Wonderlust RN To Software Engineer</h1>
+            </Carousel.Caption>
+        <Carousel.Item>
+             <img 
+                src="./images/theNarrows.jpeg"
+                alt="Zion National Park 2022" 
+            />
+            <Carousel.Caption style={{color:"black"}}>
                 <h1> Wonderlust RN To Software Engineer</h1>
             </Carousel.Caption>
         </Carousel.Item>
